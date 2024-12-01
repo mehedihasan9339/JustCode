143d5b2531d39571fa48d805d8df894790313b71
Mehedi Hasan <64748905+mehedihasan9339@users.noreply.github.com>
2024-12-02 02:23:04 +0600

    fdfd

---
+++ Leet-Code/Array/Easy/1346. Check If N and Its Double Exist/CheckIfExist.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public bool CheckIfExist(int[] arr)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length; j++)
+            {
+                if (i != j)
+                {
+                    if (arr[i] == arr[j] * 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
