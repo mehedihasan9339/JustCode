f8bc08fb9108e0f5e6239ac902a1a0a43efe0539
Mehedi Hasan <64748905+mehedihasan9339@users.noreply.github.com>
2024-12-03 06:20:08 +0600

    code

---
+++ Leet-Code/Strings/Easy/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/IsPrefixOfWord.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int IsPrefixOfWord(string sentence, string searchWord)
+    {
+        if (string.IsNullOrEmpty(sentence) || string.IsNullOrEmpty(searchWord))
+        {
+            return -1;
+        }
+        else
+        {
+            var words = sentence.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].IndexOf(searchWord) == 0)
+                {
+                    return i + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Leet-Code/Strings/Easy/2109. Adding Spaces to a String/AddSpaces.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public string AddSpaces(string s, int[] spaces) {
+         List<char> result = new List<char>();
+        int spaceIndex = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (spaceIndex < spaces.Length && i == spaces[spaceIndex])
+            {
+                result.Add(' ');
+                spaceIndex++;
+            }
+            result.Add(s[i]);
+        }
+
+        return new string(result.ToArray());
+    }
+}
