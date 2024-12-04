79d9d1d615ea377423705c8f79608b8c6b47fae5
Mehedi Hasan <64748905+mehedihasan9339@users.noreply.github.com>
2024-12-05 02:37:00 +0600

    lastt\

---
+++ Leet-Code/Strings/Medium/2825. Make String a Subsequence Using Cyclic Increments/CanMakeSubsequence.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    public bool CanMakeSubsequence(string str1, string str2) {
+        int i = 0, j = 0;
+        
+        while (i < str1.Length && j < str2.Length) {
+            if (str1[i] == str2[j] || IsCyclicIncrement(str1[i], str2[j])) {
+                j++;
+            }
+            i++;
+        }
+        
+        return j == str2.Length;
+    }
+    
+    private bool IsCyclicIncrement(char c1, char c2) {
+        return (c2 - c1 + 26) % 26 == 1;
+    }
+}
